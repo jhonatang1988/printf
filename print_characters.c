c9f13cb870302a3973020e092527ddd62017e321
Daniel E Rodriguez <dr2d4@live.com>
2019-07-28 14:39:04 -0500

    create functions print strings - chars

---
+++ print_characters.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * cb_print_string - print string
+ * @list: by get argument
+ *
+ * Return: num of char printed
+ */
+int cb_print_string(va_alist list)
+{
+	char *s = va_arg(list, char *);
+	int l = 0;
+
+	for (l = 0; *s; l++)
+		_putchar(*s);
+
+	return (l);
+}
+
+/**
+ * cb_print_char - print char
+ * @list: by get argument
+ *
+ * Return: Always 1
+ */
+int cb_print_char(va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
