7d738d34b407cc3ee7bc1a1f6485873f2ccb7c16
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-28 22:47:12 +0000

    merge the headers

--- holberton.h
+++ holberton.h
@@ -3,10 +3,25 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct print_methods - struct for all print identifiers
+ *
+ * @fid: the identifier
+ * @func: the function to return to print as needed by the identifier
+ */
+typedef struct print_methods
+{
+	char *fid;
+	int (*func)(va_list list);
+} print_methods;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*get_func(const char *s))(va_list);
 int cb_print_string(va_list list);
 int cb_print_char(va_list list);
+int cb_print_percentage(va_list list);
+
 #endif /* HOLBERTON_H */
