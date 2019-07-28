b7871d0813bc2395eb5192efc715cc21e044b486
Daniel E Rodriguez <dr2d4@live.com>
2019-07-28 12:22:08 -0500

    printf base

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 /**
  * _printf - print format string
@@ -7,5 +8,27 @@
  */
 int _printf(const char *format, ...)
 {
+	int (*f)(valist);
+	va_list list;
+
+	va_start(list, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+			printf("%c", *format++);
+		else
+		{
+			f = get_funtcion(*(++format));
+
+			if (!f)
+				printf("%%%c", *format++);
+
+			f(list);
+		}
+	}
+
+	// printf("das %d\n");
+	va_end(list);
 	return (0);
 }
