e04356af9de250d42972d240049ed17a651e68a7
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-28 20:00:01 +0000

    added the prototypes for cb_print_string and cb_print_char

--- holberton.h
+++ holberton.h
@@ -3,4 +3,6 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*get_func(char *s))(va_list);
+int cb_print_string(va_list list);
+int cb_print_char(va_list list);
 #endif /* HOLBERTON_H */
